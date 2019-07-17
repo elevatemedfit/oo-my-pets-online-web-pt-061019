41c7d9a76372e10862ddb689e9f07105bf112331
Chris Junker <elevatemedfit@gmail.com>
2019-07-17 17:12:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -67,5 +67,5 @@ attr_accessor :owner,:cats,:dogs
   end
 
   def list_pets
-  "I have #{owner.dogs.count} dog(s), and #{owner.cats.count} cat(s)."
+  puts "I have #{owner.dogs.count} dog(s), and #{owner.cats.count} cat(s)."
   end
