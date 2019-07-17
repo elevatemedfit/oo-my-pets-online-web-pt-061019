d17cc1537c2472fc126852349cd3fae30637ad1e
Chris Junker <elevatemedfit@gmail.com>
2019-07-17 16:03:08 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -31,8 +31,8 @@ attr_accessor :owner,:cats,:cat,:dog,:dogs
      #self
    end
 
-  def buy_cat(name,owner)
-    @cats = Cat.new(name,owner)
+  def buy_cat(name)
+    @cats = Cat.new(name)
   end
 
 
