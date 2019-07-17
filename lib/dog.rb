2a6e846e8260465fa1094ae1e9f44b57c7fb4235
Chris Junker <elevatemedfit@gmail.com>
2019-07-17 16:09:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  attr_reader :name, :owner
+  attr_reader :name, :owner, :dog, :dogs
   attr_accessor :mood
 
   @@all =[]
@@ -10,10 +10,6 @@ class Dog
     @mood = "nervous"
     @@all << self
     @owner.dogs << self
-  end
-
-  def add_dog(owner)
-    owner.add_dog(self)
   end
 
   def self.all
